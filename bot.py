8006cb829b29ce0b4df76888f3bbc53d53f26f69
muhammadjalilov <mjalilov2004@gmail.com>
2024-06-29 14:34:32 +0500

    pre ustozshogirdbot

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import os
 from asyncio import run
-from aiogram.filters import Command
-from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher
 
-import functions
-from functions import get_info
-from aiogram.types import BotCommand
+from aiogram.filters import Command
+from aiogram.types import BotCommand, Message
+from dotenv import load_dotenv
+
+from functions import start, info, stop, vacancy, helps, start_menu, register_name, register_phone, register_address, \
+    register_position, register_finish
+from states import SignUp
+
+from aiogram import Bot, Dispatcher
 
 load_dotenv()
 
@@ -15,30 +18,28 @@ TOKEN = os.getenv("bot_token")
 dp = Dispatcher()
 
 
-async def start(bot: Bot):
-    await bot.send_message(chat_id="779534487", text="Bot Ishga tushdi ✅")
-
-
-async def stop(bot: Bot):
-    await bot.send_message(chat_id="779534487", text="Bot To'xtadi ⚠️")
-
-
-async def main(dp_) -> None:
+async def main(dp) -> None:
     bot = Bot(token=TOKEN)
-    dp_.startup.register(start)
-
-    dp_.message.register(functions.start_answer, Command("start"))
-    dp_.message.register(functions.help_answer, Command("help"))
-    dp_.message.register(get_info)
-
-    dp_.shutdown.register(stop)
-
-    await bot.set_my_commands([
-        BotCommand(command="/start", description="Start bot!"),
-        BotCommand(command="/help", description="Help!")
-    ])
-
-    await dp_.start_polling(bot, polling_timeout=1)
+    await bot.set_my_commands(
+        [
+            BotCommand(command="/start", description="Bot ni ishga tushirish"),
+            BotCommand(command="/info", description="Shaxsiy ma'lumotlarni olish"),
+            BotCommand(command="/vacancy", description="Ishga e'lon berish"),
+            BotCommand(command="/help", description="Yordam")
+        ]
+    )
+    dp.startup.register(start)
+    dp.message.register(vacancy, Command('vacancy'))
+    dp.message.register(register_name, SignUp.name)
+    dp.message.register(register_phone, SignUp.phone)
+    dp.message.register(register_address, SignUp.address)
+    dp.message.register(register_position, SignUp.position)
+    dp.message.register(register_finish, SignUp.salary)
+    dp.message.register(info, Command('info'))
+    dp.message.register(start_menu, Command('start'))
+    dp.message.register(helps, Command('help'))
+    dp.shutdown.register(stop)
+    await dp.start_polling(bot,polling_timeout=1)
 
 
 if __name__ == "__main__":
--- functions.py
+++ functions.py
@@ -4,35 +4,92 @@ from aiogram import Bot
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
+from states import SignUp
 
-async def get_info(message: Message, bot: Bot, state: FSMContext):
+
+async def info(message: Message, bot: Bot, state: FSMContext):
     profile = await bot.get_chat(chat_id=message.from_user.id)
     user = message.from_user
     user_photos = await message.from_user.get_profile_photos()
 
-    data = user.mention_html("USER") + " INFO:\n\n"
-    data += f"""Firstname: {user.first_name} \nId: {user.id} \n"""
+    data = f"{message.from_user.mention_html('USER')}" + ' INFO:\n\n'
+    data += f"""Sizning ismingiz : {user.first_name} \nId raqamingiz: {user.id} \n"""
     if user.username:
-        data += f"Username @{user.username}\n"
+        data += f"Siznig usernameiz @{user.username}\n"
     if user.last_name:
-        data += f"Lastname: {user.last_name}\n"
+        data += f"Sizning familyangiz {user.last_name}\n"
     if profile.bio:
-        data += f"Your bio: {profile.bio}\n"
+        data += f"Sizning bioingiz {profile.bio}\n"
     if user_photos.photos:
         await message.answer_photo(user_photos.photos[0][-1].file_id, caption=data, parse_mode="HTML")
     else:
-        await message.answer(data, parse_mode="HTML")
+        await message.answer(text=data, parse_mode="HTML")
     pprint(data)
 
 
-async def start_answer(message: Message, bot: Bot):
-    await bot.send_message(message.from_user.id,
-                           text=f"Hello {message.from_user.mention_html(f'{message.from_user.first_name}')}",
-                           parse_mode="HTML")
+async def helps(message: Message, bot: Bot, state: FSMContext):
+    await message.answer("""
+/start -> Botni ishga tushirish    
+/help -> Commandlarni ko'rish    
+/vacancy -> E'lon berish    
+
+    """)
 
 
-async def help_answer(message: Message, bot: Bot):
-    await bot.send_message(message.from_user.id,
-                           text="""
-                                /start -> Start bot\n/help -> Commands
-                           """)
+async def vacancy(message: Message, bot: Bot, state: FSMContext):
+    await message.answer(
+        "Ismingnizni kiriting: "
+    )
+    await state.set_state(SignUp.name)
+
+
+async def register_name(message: Message, bot: Bot, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Telefon raqamizni kiriting: ")
+    await state.set_state(SignUp.phone)
+
+
+async def register_phone(message: Message, bot: Bot, state: FSMContext):
+    await state.update_data(phone=message.text)
+    await message.answer("Manzilni kiriting: ")
+    await state.set_state(SignUp.address)
+
+
+async def register_address(message: Message, bot: Bot, state: FSMContext):
+    await state.update_data(address=message.text)
+    await message.answer("Lavozimni kiriting: ")
+    await state.set_state(SignUp.position)
+
+
+async def register_position(message: Message, bot: Bot, state: FSMContext):
+    await state.update_data(position=message.text)
+    await message.answer("Maoshni kiriting: ")
+    await state.set_state(SignUp.salary)
+
+
+async def register_finish(message: Message, bot: Bot, state: FSMContext):
+    await state.update_data(salary=message.text)
+    data = await state.get_data()
+    txt = f'''Ma'lumotlaringiz: 
+    Ism: {data.get("name")}
+    Telefon: {data.get("phone")}
+    Manzil: {data.get("address")}
+    Lavozim: {data.get("position")}
+    <b>Maosh: {data.get("salary")} </b>
+    '''
+    await message.answer(text=txt, parse_mode="html")
+    await message.answer("Tez Orada Kanalda Chop etiladi")
+    await bot.send_message(chat_id="779534487", text=txt)
+    await state.clear()
+
+
+async def start_menu(message: Message, bot: Bot, state: FSMContext):
+    await message.answer(f"Assalomu Alaykun {message.from_user.first_name} /help orqali menularni ko'ring")
+
+
+async def start(bot: Bot):
+    await bot.send_message(chat_id="779534487", text="Bot Ishga tushdi ✅")
+
+
+async def stop(bot: Bot):
+    await bot.send_message(chat_id="779534487", text="Bot To'xtadi ⚠️")
